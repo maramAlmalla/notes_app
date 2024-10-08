aa3f35d6422805d3a7cf1b69a24cbad26b6dc924
maramAlmalla <maramalm367@gmail.com>
2024-10-08 14:47:58 +0300

    take input and validate data

--- lib/view/wedgits/add_note)modal_sheet.dart
+++ lib/view/wedgits/add_note)modal_sheet.dart
@@ -7,27 +7,66 @@ class AddNoteModalSheet extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 16),
+    return const Padding(
+      padding: EdgeInsets.symmetric(horizontal: 16),
+      child: AddNoteForm(),
+    );
+  }
+}
+
+class AddNoteForm extends StatefulWidget {
+  const AddNoteForm({
+    super.key,
+  });
+
+  @override
+  State<AddNoteForm> createState() => _AddNoteFormState();
+}
+
+class _AddNoteFormState extends State<AddNoteForm> {
+  GlobalKey<FormState> formKey = GlobalKey();
+  AutovalidateMode autovalidateMode = AutovalidateMode.disabled;
+  String? title, subTitle;
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: formKey,
+      autovalidateMode: autovalidateMode,
       child: Column(
         children: [
           const SizedBox(
             height: 32,
           ),
-          const CustomTextField(
+          CustomTextField(
+            onSaved: (value) {
+              title = value;
+            },
             hint: 'title',
           ),
           const SizedBox(
             height: 16,
           ),
-          const CustomTextField(
+          CustomTextField(
+            onSaved: (value) {
+              subTitle = value;
+            },
             hint: 'content',
             maxLines: 5,
           ),
           const SizedBox(
             height: 16,
           ),
-          CustomButton(text: 'Add'),
+          CustomButton(
+            text: 'Add',
+            onTap: () {
+              if (formKey.currentState!.validate()) {
+                formKey.currentState!.save();
+              } else {
+                autovalidateMode = AutovalidateMode.always;
+                setState(() {});
+              }
+            },
+          ),
         ],
       ),
     );
