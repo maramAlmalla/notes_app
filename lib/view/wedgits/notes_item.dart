ab71f3898baae8d161091c6fdaa6f9034a89e0cf
maramAlmalla <maramalm367@gmail.com>
2024-10-15 14:14:31 +0300

    display data

--- lib/view/wedgits/notes_item.dart
+++ lib/view/wedgits/notes_item.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:notes_app/models/note_model.dart';
 import 'package:notes_app/view/edit_note_view.dart';
 
 class NotesItem extends StatelessWidget {
-  const NotesItem({super.key});
-
+  const NotesItem({super.key, required this.note});
+  final NoteModel note;
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
@@ -17,14 +18,14 @@ class NotesItem extends StatelessWidget {
         padding: const EdgeInsets.only(top: 16, bottom: 16, left: 16),
         decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(16),
-          color: Colors.yellow,
+          color: Color(note.color),
         ),
         child: Column(
           children: [
             ListTile(
-              title: const Text(
-                'Flutter Tips',
-                style: TextStyle(
+              title: Text(
+                note.title,
+                style: const TextStyle(
                   color: Colors.black,
                   fontSize: 26,
                 ),
@@ -32,7 +33,7 @@ class NotesItem extends StatelessWidget {
               subtitle: Padding(
                 padding: const EdgeInsets.only(top: 16, bottom: 16),
                 child: Text(
-                  'Build your Career with   Maram Almalla',
+                  note.subTitle,
                   style: TextStyle(
                       color: Colors.black.withOpacity(.4), fontSize: 18),
                 ),
@@ -46,7 +47,7 @@ class NotesItem extends StatelessWidget {
             Padding(
               padding: const EdgeInsets.only(left: 200),
               child: Text(
-                'May 21, 2024',
+                note.date,
                 style: TextStyle(color: Colors.black.withOpacity(.4)),
               ),
             )
